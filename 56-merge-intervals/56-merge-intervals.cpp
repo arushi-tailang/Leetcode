b08b8dc005b488f10c46812767937e5d1df18ab9
Arushi Tailang <75250092+arushi-tailang@users.noreply.github.com>
2022-06-25 02:23:44 +0530

    Time: 71 ms (30.11%), Space: 20.1 MB (16.29%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,20 +3,18 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> mergedIntervals;
         
-        if(intervals.size()==0) return mergedIntervals;
+        sort(intervals.begin(), intervals.end());
+        vector<int> tempInterval = intervals[0];
         
-        sort(intervals.begin(),intervals.end());
-        vector<int>tempit=intervals[0];
-        for(auto it:intervals){
-         if(it[0]<=tempit[1]){
-                tempit[1]=max(it[1],tempit[1]);
+        for (auto it: intervals){
+            if(it[0]<=tempInterval[1]){
+                tempInterval[1]=max(it[1],tempInterval[1]);
             }else{
-                mergedIntervals.push_back(tempit);
-                tempit=it;
+                mergedIntervals.push_back(tempInterval);
+                tempInterval=it;
             }
         }
-        
-        mergedIntervals.push_back(tempit);
+        mergedIntervals.push_back(tempInterval);
         return mergedIntervals;
     }
 };
