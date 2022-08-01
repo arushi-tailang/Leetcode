de89948802141f5c1a314ffeb5354723a930acdc
Arushi Tailang <75250092+arushi-tailang@users.noreply.github.com>
2022-08-01 18:36:43 +0530

    Time: 4 ms (32.95%), Space: 5.8 MB (96.14%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-         double ans = 1.0;
-         long long nn = n;
-        if (nn < 0) nn = -1 * nn;
+        double ans = 1.0;
+        long long nn = n;
+        if(nn < 0) nn = -1 * nn;
+         
         
         while(nn){
             if(nn%2==1){
